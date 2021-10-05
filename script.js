fbbf78af44b3b03fc7ef5363ccca109fcf717039
Mikisali <56927963+Mikisali@users.noreply.github.com>
2021-10-05 14:31:58 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $( "img" ).click(function() {
-        for (var i = 0; i < 5; i++){
-            $(this).slideUp();
-            $(this).slideDown();
-        }
-    });
-}); 
